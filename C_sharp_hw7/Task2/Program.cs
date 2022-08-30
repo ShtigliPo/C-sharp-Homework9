b80c15361b7d83462388aa1559fa40f936721f98
Alexey <forsage@list.ru>
2022-08-30 20:31:55 +0300

    Задачи 7 дз готовы

---
+++ C_sharp_hw7/Task2/Program.cs
@@ -0,0 +1,69 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string readValue = Console.ReadLine();
+    int result = int.Parse(readValue);
+    return result;
+}
+
+int[,] FillArray(int line, int column) 
+{
+    int[,] matrix = new int[line, column];
+    for (int i = 0; i < matrix.GetLength(0); i++) 
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) 
+        {
+            matrix[i, j] = (int)(new Random().Next(-9, 10));
+        }
+    }
+    return matrix;
+}
+
+void PrintArray(int[,] matrix) 
+{
+    for (int i = 0; i < matrix.GetLength(0); i++) 
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) 
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+bool CheckIndexes(int[,] matrix, int line, int column)
+{
+    bool result = true;
+    if (matrix.GetLength(0) <= line || matrix.GetLength(1) <= column)
+    {
+        result = false;
+    }
+    return result;
+}
+
+void FindElement(int[,] matrix, int line, int column) 
+{
+    if (CheckIndexes(matrix, line, column))  
+    {
+        System.Console.Write($"В этом индексе число {matrix[line, column]}");
+    }
+    else
+    {
+        System.Console.Write("Такого элемента нет");
+    }
+}
+
+int line = Prompt("Введите количество строк массива ");
+int column = Prompt("Введите количество столбцов массива ");
+int[,] matrix = FillArray(line, column); 
+PrintArray(matrix);
+FindElement(matrix, Prompt("Введите индекс строки "), Prompt("Введите индекс столбца "));
+System.Console.WriteLine();
---
+++ C_sharp_hw7/Task4/Program.cs
@@ -0,0 +1,81 @@
+﻿// Найдите максимальное значение в матрице по каждой строке, ссумируйте их. 
+// Затем найдети минимальное значение по каждой колонке, тоже ссумируйте их. 
+// Затем из первой суммы (с максимумами) вычтите вторую сумму(с минимумами)
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string readValue = Console.ReadLine();
+    int result = int.Parse(readValue);
+    return result;
+}
+
+int[,] FillArray(int line, int column) 
+{
+    int[,] matrix = new int[line, column];
+    for (int i = 0; i < matrix.GetLength(0); i++) 
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) 
+        {
+            matrix[i, j] = (int)(new Random().Next(0, 10));
+        }
+    }
+    return matrix;
+}
+
+void PrintArray(int[,] matrix) 
+{
+    for (int i = 0; i < matrix.GetLength(0); i++) 
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) 
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int SumLineMax(int[,] matrix) 
+{
+    int max = 0;
+    int result = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        max = matrix[i, 0];
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if(matrix[i, j] > max)
+            {
+                max = matrix[i, j];
+            }
+        }
+        result += max;
+    }
+    return result;
+}
+
+int SumColumnMin(int[,] matrix) 
+{
+    int min = 0;
+    int result = 0;
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        min = matrix[0, i];
+        for (int j = 0; j < matrix.GetLength(0); j++)
+        {
+            if(matrix[j, i] < min)
+            {
+                min = matrix[j, i];
+            }
+        }
+        result += min;
+    }
+    return result;
+}
+
+int line = Prompt("Введите количество строк массива ");
+int column = Prompt("Введите количество столбцов массива ");
+int[,] matrix = FillArray(line, column); 
+PrintArray(matrix);
+System.Console.WriteLine($"Разность сумм равна {SumLineMax(matrix) - SumColumnMin(matrix)}");
+System.Console.WriteLine(SumLineMax(matrix) - SumColumnMin(matrix));
